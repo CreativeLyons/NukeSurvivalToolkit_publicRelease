9f73d634db63036e454532733339be109124a6c8
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2022-02-18 00:15:20 +0100

    add RadialDilate CF

--- NukeSurvivalToolkit/menu.py
+++ NukeSurvivalToolkit/menu.py
@@ -350,6 +350,7 @@ filterMenu.addCommand('CatsEyeDefocus NKPD', "nuke.createNode('{}CatsEyeDefocus'
 filterMenu.addCommand('DefocusSwirlyBokeh NKPD', "nuke.createNode('{}DefocusSwirlyBokeh')".format(prefixNST), icon="nukepedia_icon.png")
 filterMenu.addCommand('deHaze NKPD', "nuke.createNode('{}deHaze')".format(prefixNST), icon="nukepedia_icon.png")
 filterMenu.addCommand('RankFilter JP', "nuke.createNode('{}RankFilter')".format(prefixNST), icon="Median.png")
+filterMenu.addCommand('RadialDilate CF', "nuke.createNode('{}RadialDilate')".format(prefixNST), icon="ErodeFast.png")
 filterMenu.addCommand('DeflickerVelocity NKPD', "nuke.createNode('{}DeflickerVelocity')".format(prefixNST), icon="nukepedia_icon.png")
 filterMenu.addCommand('FillSampler NKPD', "nuke.createNode('{}FillSampler')".format(prefixNST), icon="nukepedia_icon.png")
 filterMenu.addCommand('MECfiller NKPD', "nuke.createNode('{}MECfiller')".format(prefixNST), icon="nukepedia_icon.png")
